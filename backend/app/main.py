b48d770ece72aae6541d1218f32e571d6d09d47f
Azka <azka.dev@gmail.com>
2026-09-09 04:32:08 -0700

    change in main

--- backend/app/main.py
+++ backend/app/main.py
@@ -50,7 +50,7 @@ app = FastAPI(
 frontend_url = os.getenv("FRONTEND_URL", "http://localhost:3000")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[frontend_url, "http://localhost:3000", "http://localhost:3001"],
+    allow_origins=[frontend_url, "https://ai-meeting-assistant-azkacpu.vercel.app/", "https://ai-meeting-assistant-azkacpu.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
